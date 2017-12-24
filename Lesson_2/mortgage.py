f8beaa2405f43e24e839e9470a89e8deb86d370a
jdwy215@gmail.com <jdwy215@gmail.com>
2017-12-24 14:18:56 -0500

    adding formatted printing to state the inputs mortgage.py

--- Lesson_2/mortgage.py
+++ Lesson_2/mortgage.py
@@ -3,7 +3,14 @@
 # Find out how long to pay off a mortgage 
 import argparse
 
+def print_schedule(principal, payment, rate, total_paid):
+    """Idea would to print the schedule regardless and send the yrs_to_pay here
+    just to print"""
+
+
 def calc_yrs(principal=500000, payment=2684.11, rate=0.05, total_paid=0):
+    print("Inputs:\nPrinicpal: {:<10.2f} Monthly Payment: {:<10.2f} Interest Rate: {:<10.2f} Down Payment: {:<10.2f} \n".format(principal, payment, rate, total_paid))
+
     while principal > 0:
         interest = (rate / 12) * principal
         principal = (principal + interest) - payment
@@ -11,6 +18,7 @@ def calc_yrs(principal=500000, payment=2684.11, rate=0.05, total_paid=0):
 
     yrs_to_pay = total_paid/(payment*12)
     print("It will take {0:0.1f} years to pay off your mortgage".format(yrs_to_pay))
+    print("The total cost of the mortgage will be {0:0.1f}.".format(total_paid))
 
 def main():
     #principal = 500000
@@ -18,7 +26,7 @@ def main():
     #rate = 0.05
     #total_paid = 0
     #calc_yrs(principal, payment, rate, total_paid)
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Input arguments for mortgage calculator')
     parser.add_argument('-principal', help="starting total mortgage balance")
     parser.add_argument('-payment', help="total mortgage pmt per month")
     parser.add_argument('-rate', help="interest rate (if 5%, input as 0.05")
