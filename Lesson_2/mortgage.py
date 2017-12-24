1e0232c12d8ca4aa78874eca92800252b40e463c
jdwy215@gmail.com <jdwy215@gmail.com>
2017-12-23 23:19:33 -0500

    adding argument parsing and try except

--- Lesson_2/mortgage.py
+++ Lesson_2/mortgage.py
@@ -18,7 +18,24 @@ def main():
     #rate = 0.05
     #total_paid = 0
     #calc_yrs(principal, payment, rate, total_paid)
-    calc_yrs()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-principal', help="starting total mortgage balance")
+    parser.add_argument('-payment', help="total mortgage pmt per month")
+    parser.add_argument('-rate', help="interest rate (if 5%, input as 0.05")
+    parser.add_argument('-total_paid', help="total down payment")
+    args = parser.parse_args()
+
+    #this is not working because all parameters dont have values 
+    try:
+        if args:
+            calc_yrs(int(args.principal), float(args.payment),
+            float(args.rate), float(args.total_paid))
+        else:
+            calc_yrs()
+    except Exception:
+        print("Without specifying all values, the script will run with defaults.")
+        print("Running with default params...\n")
+        calc_yrs()
 
 if __name__ == "__main__":
     main()
